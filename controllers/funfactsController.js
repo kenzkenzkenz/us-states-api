648fdbdf00fc0cc082c2d5d876f8748f2eceb238
Kenzie <kenzieli23@gmail.com>
2022-05-09 15:30:11 -0500

    not working

--- controllers/funfactsController.js
+++ controllers/funfactsController.js
@@ -29,27 +29,28 @@ const getFunfact = async (req, res) => {
 // POST a fun fact
 const createFunfact = async (req, res) => {
     //dealing with inputs
-    if (!req?.body?.stateCode || !req?.body?.funfacts) {
+    if (!req?.body?.funfacts) {
         return res.status(400).json({ 'message': 'State fun facts value required' });
     }
     const myFunfacts = req.body.funfacts;
     if (!Array.isArray(myFunfacts)){
         return res.status(400).json({ 'message': 'State fun facts value must be an array' });
     }
-    const myStateCode = req.body.stateCode;
+    const myStateCode = req.params.stateCode;
     let factsArray = [];
 
     //dealing with mongo
     const mongoData = await mongoFunFacts.find();
     const stateExists = mongoData.find(st => st.stateCode === myStateCode);
     if (stateExists) {
-        factsArray = [...stateExists.funfacts, myFunfacts];
+        factsArray = [...stateExists, myFunfacts];
+        stateExists.funfacts = factsArray
         const result = await stateExists.save();
         return res.status(200).json(result);
     }
 
     const result = await mongoFunFacts.create({
-        stateCode: req.body.stateCode,
+        stateCode: req.params.stateCode,
         funfacts: req.body.funfacts
     });
     res.json(result);
@@ -58,7 +59,7 @@ const createFunfact = async (req, res) => {
 // PATCH a fun fact
 const updateFunfact = async (req, res) => {
     //deal with inputs
-    if (!req?.body?.stateCode || !req?.body?.index || !req?.body?.funfact) {
+    if (!req?.body?.index || !req?.body?.funfact) {
         return res.status(400).json({ 'message': 'State fun fact index value required' });
     }
     let myIndex = req.body.index - 1;
@@ -71,19 +72,19 @@ const updateFunfact = async (req, res) => {
     //deal with mongo
     const mongoData = await mongoFunFacts.find();
 
-    const state = await mongoData.find(st => st.stateCode === req.body.stateCode);
+    const state = await mongoData.find(st => st.stateCode === req.params.stateCode);
     if (!state) {
         return res.status(204).json({ "message": `No Fun Facts found for ${state}.` });
     }
 
     const myStateFactsArray = state.map(st => st.funfacts);
     if (!myStateFactsArray[req.body.index]) {
-        return res.status(204).json({  "message": `No Fun Fact found at that index for ${state.state}.` });
+        return res.status(204).json({  "message": `No Fun Fact found at that index for ${state}.` });
     }
-    if (req.body?.stateCode) {
-        state.stateCode = req.body.stateCode;
+    if (req.params.stateCode) {
+        state.stateCode = req.params.stateCode;
     }
-    if (req.body?.index) {
+    if (req.body.index) {
         state.funfacts[myIndex] = req.body.funfact;
     }
 
@@ -93,25 +94,25 @@ const updateFunfact = async (req, res) => {
 
 // DELETE a fun fact
 const deleteFunfact = async (req, res) => {
-    if (!req?.body?.stateCode || !req?.body?.index) { //if missing inputs
+    if (!req?.body?.index) { //if missing inputs
         return res.status(400).json({ 'message': `State fun fact index value required` });
     }
     let myIndex = req.body.index - 1; //assign index
-    const state = await mongoFunFacts.findOne({ stateCode: req.body.stateCode }).exec(); //look for match
+    const state = await mongoFunFacts.findOne({ stateCode: req.params.stateCode }).exec(); //look for match
 
     if (!state) { // if no state matches
-        return res.status(204).json({ "message": `No Fun Facts found for ${req.body.stateCode}.` });
+        return res.status(204).json({ "message": `No Fun Facts found for ${req.params.stateCode}.` });
     }
 
     let factsArray = [...state.funfacts]; //create array of chosen state's facts only
     if (!factsArray[myIndex]) {
-        return res.status(204).json({  "message": `No Fun Fact found at that index for ${state.state}.` });
+        return res.status(204).json({  "message": `No Fun Fact found at that index for ${state}.` });
     }
 
     factsArray = factsArray.filter(fact => fact !== factsArray[myIndex]); //filter out selected index
     state.funfacts = factsArray; // reassign state funfacts
     const result = await state.save(); //save results
-    return res.status(200).json({"message": `The funfact at index ${req.body.index} has been deleted.`});//print success message
+    res.json(result);//print success message
 }
 
 module.exports = {
